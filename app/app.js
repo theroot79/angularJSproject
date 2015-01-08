523868652683a0be6df643dbc4cf7807e8dd0625
Vasil Tsintsev <v.tsintsev@gmail.com>
2015-01-08 20:47:41 +0200

    0.2 - Rebuilding the main structure

--- app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('angularJSapp',[
-  'angularJSapp.homepage'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/'});
-}]);
